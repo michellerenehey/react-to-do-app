d74de6d0769c754c8d3ad58f27f1ff27f88e8b15
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-10 17:47:25 -0800

    set up services folder with base functions

---
+++ src/services/users.js
@@ -0,0 +1,32 @@
+import { client, checkError } from './client.js';
+
+// function to get user session
+export async function getUser() {
+  return client.auth.session();
+}
+// function to sign up
+export async function signUpUser(email, password) {
+  const { user, error } = await client.auth.signUp({ email, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+// function to sign in
+export async function signInUser(email, password) {
+  const { user, error } = await client.auth.signIn({ email, password });
+  if (error) {
+    throw error;
+  }
+  return user;
+}
+// function to logout
+export async function logout() {
+  const response = await client.auth.signOut();
+  return checkError(response);
+}
+// function to create to-dos
+export async function createToDo(task) {
+  const resp = await client.from('todos').insert([{ task: task, user_id: client.auth.user().id }]);
+  return checkError(resp);
+}
