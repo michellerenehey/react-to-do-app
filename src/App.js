0c7c1dd63577e1bd8ff3f17f7d1fc4fce13526c8
Michelle Nygren <michelle.r.nygren@gmail.com>
2022-01-11 14:45:56 -0800

    continued setting up file stucture, pulled auth and authform file from registration project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { useState } from 'react';
+import { getUser, logout } from './services/users';
+import Auth from '../src/views/Auth/Auth';
 
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(getUser());
+  const logoutUser = async () => {
+    await logout();
+    setCurrentUser(null);
+  };
   return (
     <div className="App">
+      <h1>To Do List</h1>
       <BrowserRouter>
         <Switch>
-          <Route></Route>
+          <Route exact path="/">
+            {currentUser && (
+              <>
+                <p>current logged in user</p>
+                <button onClick={logoutUser}>Log Out</button>
+              </>
+            )}
+            {!currentUser && <Auth setCurrentUser={setCurrentUser} />}
+          </Route>
         </Switch>
       </BrowserRouter>
     </div>
--- src/views/Auth/Auth.js
+++ src/views/Auth/Auth.js
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import AuthForm from '../../components/AuthForm/AuthForm';
+import { signInUser, signUpUser } from '../../services/users';
 import './Auth.css';
+import classNames from 'classnames';
 
-export default function Auth() {
-  return <div>auth component</div>;
+export default function Auth({ setCurrentUser }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState(null);
+  const [type, setType] = useState('signin');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response =
+        type === 'signin' ? await signInUser(email, password) : await signUpUser(email, password);
+      setCurrentUser(response);
+    } catch {
+      setMessage('Something went wrong, try again!');
+    }
+  };
+  return (
+    <div>
+      <div className="top">
+        <h3 onClick={() => setType('signin')} className={classNames({ active: type === 'signin' })}>
+          Sign In
+        </h3>
+        <h3 onClick={() => setType('signup')} className={classNames({ active: type === 'signup' })}>
+          Sign Up
+        </h3>
+      </div>
+      <AuthForm
+        message={message}
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        handleSubmit={handleSubmit}
+      />
+    </div>
+  );
 }
